1032458061ae697419d4237951254743023bdf41
yanivRichman <yaniv4980@gmail.com>
2022-01-27 16:23:39 +0200

    book responsive

--- js/apps/books/cmps/book-filter.cmp.js
+++ js/apps/books/cmps/book-filter.cmp.js
@@ -1,19 +1,18 @@
 export default {
     template: `
         <div class="book-filter">
-            <label></label>
-            <input @input="filter" v-model="filterBy.title" type="text" placeholder="Search book">
-            <label>From price</label>
-            <input @input="filter" v-model.number="filterBy.fromPrice" type="number" placeholder="Search...">
-            <label>To price</label>
-            <input @input="filter" v-model.number="filterBy.toPrice" type="number" placeholder="Search...">
+            <input @input="filter" v-model="filterBy.title" type="text" placeholder="Search a book..">
+            <label> | </label>
+            <input @input="filter" v-model.number="filterBy.fromPrice" type="number" placeholder="From price...">
+            <label> | </label>
+            <input @input="filter" v-model.number="filterBy.toPrice" type="number" placeholder="To price...">
         </div>
     `,
     data() {
         return {
             filterBy: {
                 title: '',
-                fromPrice: 0,
+                fromPrice: '',
                 toPrice: Infinity,
             }
         };
